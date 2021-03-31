d6a629b2ee56964addc885f86a727f193fa2b31b
HobbitWarrior <haim.hddd@gmail.com>
2021-03-31 20:30:53 +0300

    init

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+function Ingredients() {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredient => [
+      ...prevIngredient, 
+      {id:Math.random().toString(), ...ingredient}
+    ]);
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}}/>
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
